f8f874ead2cf02c35ca037b8677f5d6954caf9f2
kjablonski08@gmail.com <kjablonski08@gmail.com>
2020-11-17 13:34:04 -0500

    added routes for components to app.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,29 @@
 import React, { useState, useEffect } from 'react';
+import { Route, Switch } from 'react-router-dom';
 import './App.css';
 import Nav from './components/Nav/nav';
-
+import Home from './components/home/home';
 import Favorites from './components/favorites/favorites';
 import GiftForm from './components/giftForm/giftForm';
 import GiftList from './components/giftList/giftList';
+import GiftShow from './components/giftShow/giftShow';
 
 function App() {
 	const [gifts, setGifts] = useState([]);
 
 	return (
 		<div className='App'>
-			<Nav />
-			my squad is dope i can second that.
+			<header>
+				<Nav />
+			</header>
+			<main></main>
+			<Switch>
+				<Route exact path='/' component={Home} />
+				<Route path='/gifts' component={GiftList} />
+				<Route path='/gift/:id' component={GiftShow} />
+				<Route path='/add-gift' component={GiftForm} />
+				<Route path='/favorites' component={Favorites} />
+			</Switch>
 		</div>
 	);
 }
