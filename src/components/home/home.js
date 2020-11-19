ff6f21baeff98e5612551617155b418f75303569
lizbarnett0 <lizcroper@gmail.com>
2020-11-19 15:24:50 -0600

    homepage structure built

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { Card, Button } from 'react-bootstrap';
 import './home.css';
+import StockPhoto from '../../stockphoto.png';
 
 const Home = () => {
 	const giftCategories = [
@@ -27,15 +28,28 @@ const Home = () => {
 			<div className='welcome-area'>
 				<h1>welcome.</h1>
 				<h3> find the perfect gift for everyone on your list.</h3>
-				<Button href='#category' variant='outline-primary'>
-					Browse by Category
-				</Button>
-				<Button href='#price' variant='outline-primary'>
-					Browse by Price
-				</Button>
+				<div id='img-container'>
+					<img src={StockPhoto} />
+				</div>
+				<div id="button-div">
+					<Button
+						className='home-button'
+						href='#category'
+						variant='outline-primary'
+						size='lg'>
+						Browse by Category
+					</Button>
+					<Button
+						className='home-button'
+						href='#price'
+						variant='outline-primary'
+						size='lg'>
+						Browse by Price
+					</Button>
+				</div>
 			</div>
 
-			<div id="category" className='section-container'>
+			<div id='category' className='section-container'>
 				<h3>Browse Gifts By Category</h3>
 				<div className='category-area'>
 					{giftCategories.map((category) => {
