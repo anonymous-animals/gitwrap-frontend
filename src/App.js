f45ffd2a5121b329bd54f0af14cc766121a4fe00
kjablonski08@gmail.com <kjablonski08@gmail.com>
2020-11-20 22:28:28 -0500

    login form and add user form added

--- src/App.js
+++ src/App.js
@@ -1,84 +1,99 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { Route, Switch } from 'react-router-dom';
 import NotFound from './components/notFound/notFound';
+import UserForm from './components/userForm/userForm';
 import './App.css';
 
 import Nav from './components/Nav/nav';
 import Home from './components/home/home';
-import Favorites from './components/favorites/favorites'
+import Favorites from './components/favorites/favorites';
 import GiftForm from './components/giftForm/giftForm';
 import GiftList from './components/giftList/giftList';
 import GiftShow from './components/giftShow/giftShow';
-import GiftListPrice from './components/giftListPrice/giftListPrice'
-import Login from './components/login/login'
+import GiftListPrice from './components/giftListPrice/giftListPrice';
+import Login from './components/login/login';
 
 function App() {
 	const [gifts, setGifts] = useState([]);
-  const [favorites, setFavorites] = useState([]);
-  const [token, setToken] = useState('')
-  const [loggedIn, setLoggedIn] = useState(false)
-  
-  // if (token) {
-  //   setLoggedIn(true)
-  // }
+	const [favorites, setFavorites] = useState([]);
+	const [token, setToken] = useState(null);
+	const [loggedIn, setLoggedIn] = useState(false);
 
 	return (
 		<div className='App'>
-			<header>
-				<Nav />
-			</header>
-			<main>
-				<Switch>
-					<Route exact path='/' render={() => <Home />} />
-					<Route path='/add-gift' component={GiftForm} />
-					<Route
-						path='/login'
-						render={() => <Login token={token} setToken={setToken} loggedIn={loggedIn}/>}
-					/>
-					<Route
-						exact
-						path='/favorites'
-						render={() => <Favorites favorites={favorites} />}
-					/>
-					<Route
-						exact
-						path='/gifts/:id'
-						render={(routerProps) => (
-							<GiftShow
-								match={routerProps.match}
-								favorites={favorites}
-								setFavorites={setFavorites}
-								gifts={gifts}
-								setGifts={setGifts}
+			{!loggedIn ? (
+				<Login token={token} setToken={setToken} setLoggedIn={setLoggedIn} />
+			) : (
+				<div>
+					<header>
+						<Nav
+							loggedIn={loggedIn}
+							setToken={setToken}
+							setLoggedIn={setLoggedIn}
+						/>
+					</header>
+					<main>
+						<Switch>
+							<Route exact path='/' render={() => <Home token={token} />} />
+							<Route path='/new-user' component={UserForm} />
+							<Route path='/add-gift' component={GiftForm} />
+							<Route
+								path='/login'
+								render={() => (
+									<Login
+										token={token}
+										setToken={setToken}
+										loggedIn={loggedIn}
+										setLoggedIn={setLoggedIn}
+									/>
+								)}
 							/>
-						)}
-					/>
-					<Route
-						exact
-						path='/category/:category'
-						render={(routerProps) => (
-							<GiftList
-								gifts={gifts}
-								setGifts={setGifts}
-                match={routerProps.match}
-                token={token}
+							<Route
+								exact
+								path='/favorites'
+								render={() => <Favorites favorites={favorites} />}
 							/>
-						)}
-					/>
-					<Route
-						exact
-						path='/price/:price'
-						render={(routerProps) => (
-							<GiftListPrice
-								gifts={gifts}
-								setGifts={setGifts}
-								match={routerProps.match}
+							<Route
+								exact
+								path='/gifts/:id'
+								render={(routerProps) => (
+									<GiftShow
+										match={routerProps.match}
+										favorites={favorites}
+										setFavorites={setFavorites}
+										gifts={gifts}
+										setGifts={setGifts}
+									/>
+								)}
 							/>
-						)}
-					/>
-					<Route component={NotFound} />
-				</Switch>
-			</main>
+							<Route
+								exact
+								path='/category/:category'
+								render={(routerProps) => (
+									<GiftList
+										gifts={gifts}
+										setGifts={setGifts}
+										match={routerProps.match}
+										token={token}
+									/>
+								)}
+							/>
+							<Route
+								exact
+								path='/price/:price'
+								render={(routerProps) => (
+									<GiftListPrice
+										gifts={gifts}
+										setGifts={setGifts}
+										match={routerProps.match}
+									/>
+								)}
+							/>
+							<Route component={NotFound} />
+						</Switch>
+					</main>
+				</div>
+			)}
 		</div>
 	);
 }
