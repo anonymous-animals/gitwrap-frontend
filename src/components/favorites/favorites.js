c7ade924f53fb940b7a22c76ddcb2020f5a7b56d
atlfinesse <finesse.cap@gmail.com>
2020-11-21 23:35:11 -0800

    Finished favorites

--- src/components/favorites/favorites.js
+++ src/components/favorites/favorites.js
@@ -1,12 +1,40 @@
-import React from 'react';
+import React,  { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import './favorites.css'
 
 const Favorites = ({ favorites }) => {
+	const [totalPrice, setTotalPrice] = useState(0)
+	console.log(favorites)
+
+	let total = 0
+	if (favorites) {
+		for (let i = 0; i < favorites.length; i++) {
+			total += favorites[i].price
+		}
+	}
+
+	useEffect(() => {
+		setTotalPrice(total.toFixed(2))
+	}, [])
+
+
 	return (
 		<div>
 			<h1>Favorites</h1>
-			<div className='favorites-container'>
+			<div className='large-container'>
+			<div>
+				{favorites.map((favorite) => {
+					return(
+						<div key={favorite.name}>
+							<h4>{favorite.name}</h4>
+							<p>{favorite.price}</p>
+						</div>
+					)
+				})}
+				<h2>Total: ${totalPrice}</h2>
+			</div>
+
+			<div className='card-container'>
 				{favorites.map((favorite) => {
 					return (
 						<div className='card' key={favorite.name}>
@@ -14,11 +42,13 @@ const Favorites = ({ favorites }) => {
 								<Link to={`/gifts/${favorite.id}`}>
 									<img src={favorite.image} alt={favorite.name}/>
 									<p className='favorite-card-title'>{favorite.name}</p>
+									<p>${favorite.price}</p>
 								</Link>
 							</div>
 						</div>
 					);
 				})}
+			</div>
 			</div>
 		</div>
 	);
