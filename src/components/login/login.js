292eb1e075e44130d1cd7bc10a0e0d1d0c2a228c
helloxpluto <deplanetart@gmail.com>
2020-11-23 19:35:38 -0800

    added background on login
    Co-authored-by: liz <lizcroper@gmail.com>

--- src/components/login/login.js
+++ src/components/login/login.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { Redirect } from 'react-router-dom';
-import { Form, Button, Modal } from 'react-bootstrap';
+import { Form, Button, Modal, Container, Row, Col } from 'react-bootstrap';
 import Home from '../home/home';
 import NewUser from '../userForm/userForm';
 import axios from 'axios';
@@ -55,58 +55,68 @@ const Login = ({ setToken, setLoggedIn, loggedIn }) => {
 			<div className='img-container'>
 				<img src={Logo} />
 			</div>
-			<div className='userFormContainer'>
-				<h5>Login to Find the Perfect Gift!</h5>
-				<h5 className='error'>{error}</h5>
-				<Modal
-					show={showModal}
-					onHide={handleClose}
-					backdrop='static'
-					keyboard={false}
-					className='modal-form'>
-					<NewUser handleClose={handleClose} />
-				</Modal>
-				<Form onSubmit={handleSubmit} className='userForm'>
-					<Form.Group>
-						<Form.Label> Username </Form.Label>
-						<Form.Control
-							type='username'
-							name='username'
-							onChange={handleChange}
-							value={user.username}
-							placeholder='Enter Username'
-						/>
-						<Form.Label>E-mail</Form.Label>
-						<Form.Control
-							type='email'
-							name='email'
-							onChange={handleChange}
-							value={user.email}
-							placeholder='user@example.com'
-						/>
-						<Form.Label>Password</Form.Label>
-						<Form.Control
-							type='password'
-							name='password'
-							onChange={handleChange}
-							value={user.password}
-							placeholder='Enter Password'
-						/>
-					</Form.Group>
-					<div className='button-container'>
-						<Button
-							variant='primary'
-							className='login-button'
-							type='submit'
-							onClick={handleSubmit}>
-							Login
-						</Button>
-						<Button className='login-button' onClick={handleShow}>
-							Sign Up
-						</Button>
-					</div>
-				</Form>
-			</div>
+			<Container>
+				<Row>
+					<Col xs={{ order: 'last' }}></Col>
+					<Col xs>
+						<div className='userFormContainerTwo'>
+							<div className='userFormContainer'>
+								<h5>Login to Find the Perfect Gift!</h5>
+								<h5 className='error'>{error}</h5>
+								<Modal
+									show={showModal}
+									onHide={handleClose}
+									backdrop='static'
+									keyboard={false}
+									className='modal-form'>
+									<NewUser handleClose={handleClose} />
+								</Modal>
+								<Form onSubmit={handleSubmit} className='userForm'>
+									<Form.Group>
+										<Form.Label> Username </Form.Label>
+										<Form.Control
+											type='username'
+											name='username'
+											onChange={handleChange}
+											value={user.username}
+											placeholder='Enter Username'
+										/>
+										<Form.Label>E-mail</Form.Label>
+										<Form.Control
+											type='email'
+											name='email'
+											onChange={handleChange}
+											value={user.email}
+											placeholder='user@example.com'
+										/>
+										<Form.Label>Password</Form.Label>
+										<Form.Control
+											type='password'
+											name='password'
+											onChange={handleChange}
+											value={user.password}
+											placeholder='Enter Password'
+										/>
+									</Form.Group>
+									<div className='button-container'>
+										<Button
+											variant='primary'
+											className='login-button'
+											type='submit'
+											onClick={handleSubmit}>
+											Login
+										</Button>
+										<Button className='login-button' onClick={handleShow}>
+											Sign Up
+										</Button>
+									</div>
+								</Form>
+							</div>
+						</div>
+					</Col>
+					<Col xs={{ order: 'first' }}></Col>
+				</Row>
+			</Container>
 		</div>
 	);
 };
